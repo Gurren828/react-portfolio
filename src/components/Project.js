221516de4a6509c782d5bcced734268d8b5616ca
Ruben Burrell <rubenburrell828@gmail.com>
2020-05-14 00:00:16 -0700

    restored images

--- src/components/Project.js
+++ src/components/Project.js
@@ -23,7 +23,7 @@ const projects = [
     title: "Resume",
     deployedlink: "assets/resume.pdf",
     repolink: "assets/resume.pdf",
-    image: "https://lh3.googleusercontent.com/proxy/doEXa6UWRTYc29hjwuLozEcU-bv1-n6UjHs4y-DCCIAE2XZ_00up2GW9Et9qP_5K3OyXtzjX_o8ZcuzP0fN_ITKRxxPsq287K0ruLprOXkKYZlNT4g"
+    image: "assets/images/profile.png"
   }
 
 ];
@@ -37,7 +37,7 @@ function Project() {
             <img src={project.image} alt={project.title} />
           </a>
           <div id="absolute">
-            <strong>{project.title}</strong><a href={project.repolink}><img src="https://banner2.cleanpng.com/20180824/jtl/kisspng-computer-icons-logo-portable-network-graphics-clip-icons-for-free-iconza-circle-social-5b7fe46b0bac53.1999041115351082030478.jpg" alt="GitHub icon" /></a>
+            <strong>{project.title}</strong><a href={project.repolink}><img src="assets/images/profile.png" alt="Me" /></a>
           </div>
         </section>
       ))}
