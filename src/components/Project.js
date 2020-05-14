b4372c475d3f544d9a37c8c0ea1e3dc192cef2af
Ruben Burrell <rubenburrell828@gmail.com>
2020-05-13 23:52:37 -0700

    fixed images

--- src/components/Project.js
+++ src/components/Project.js
@@ -18,7 +18,14 @@ const projects = [
     deployedlink: "assets/images/employee-tracker-preview.png",
     repolink: "https://github.com/Gurren828/mysql-mployee-tracker",
     image: "assets/images/employee-tracker-preview.png"
+  },
+  {
+    title: "Resume",
+    deployedlink: "assets/resume.pdf",
+    repolink: "assets/resume.pdf",
+    image: "https://lh3.googleusercontent.com/proxy/doEXa6UWRTYc29hjwuLozEcU-bv1-n6UjHs4y-DCCIAE2XZ_00up2GW9Et9qP_5K3OyXtzjX_o8ZcuzP0fN_ITKRxxPsq287K0ruLprOXkKYZlNT4g"
   }
+
 ];
 
 function Project() {
@@ -30,7 +37,7 @@ function Project() {
             <img src={project.image} alt={project.title} />
           </a>
           <div id="absolute">
-            <strong>{project.title}</strong><a href={project.repolink}><img src="assets/images/github-icon.png" alt="GitHub icon" /></a>
+            <strong>{project.title}</strong><a href={project.repolink}><img src="https://banner2.cleanpng.com/20180824/jtl/kisspng-computer-icons-logo-portable-network-graphics-clip-icons-for-free-iconza-circle-social-5b7fe46b0bac53.1999041115351082030478.jpg" alt="GitHub icon" /></a>
           </div>
         </section>
       ))}
